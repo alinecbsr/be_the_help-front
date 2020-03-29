ff5f2a87ad13b42a564c6686a14376cff37ef3df
alinecbsr <alinecbsr@gmail.com>
2020-03-29 12:46:06 -0300

    Introduction to concepts of components, properties and state

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+import Header from './Header';
+
+function App() {
+  let [counter, setCounter ]= useState(0);
+
+  function increment() {
+    setCounter(counter += 1);
+
+    console.log(counter);
+  }
+
+  return (
+<div>
+  <Header>Counter: {counter}</Header>
+  <button onClick={increment}>Increment</button>
+</div>
+  );
+}
+
+export default App;
