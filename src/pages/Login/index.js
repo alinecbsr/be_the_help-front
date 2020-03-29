6642df86806b46ee638edca7990115aa977035d1
alinecbsr <alinecbsr@gmail.com>
2020-03-29 18:48:10 -0300

    Implemented login and styling screen

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 
+import image from '../../assets/image.png';
+import logo from '../../assets/BeTheHelp.svg';
+
 export default function Login() {
-  return <h1>Heloo React</h1>;
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img className="logo" src={logo} alt="Be the Help logo" />
+        <form>
+          <h1>Login</h1>
+          <input placeholder="Your  ID" />
+          <button className="button" type="submit">Go in</button>
+          <a href="/register">
+            <FiLogIn size={16} color="#EA80FC" />
+            I have no registration
+          </a>
+        </form>
+      </section>
+      <img className="picture" src={image} alt="Hearts in hands outstretched towards clouds in the sky." />
+    </div>
+  );
 }
