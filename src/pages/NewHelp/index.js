881568a0c1fadaa19a8bf25b93ff5f469ac7f78e
alinecbsr <alinecbsr@gmail.com>
2020-03-31 02:10:02 -0300

    Registering new help

--- src/pages/NewHelp/index.js
+++ src/pages/NewHelp/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
+
+import api from '../../services/api';
 
 import logo from '../../assets/BeTheHelp.svg';
 
 export default function NewHelp() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewHelp(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('help', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Error registering case, try again.');
+    }
+  }
+
   return (
     <div className="new-help-container">
       <div className="content">
@@ -20,10 +52,22 @@ export default function NewHelp() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Case title" />
-          <textarea placeholder="Description" />
-          <input placeholder="Value" />
+        <form onSubmit={handleNewHelp}>
+          <input
+            placeholder="Case title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Value"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <button className="button" type="submit">Register</button>
         </form>
