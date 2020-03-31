374b93913e3125ad8697a9666e73c84d969534b6
alinecbsr <alinecbsr@gmail.com>
2020-03-31 01:49:30 -0300

    User logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -11,8 +11,11 @@ import logo from '../../assets/BeTheHelp.svg';
 export default function Profile() {
   const [helps, setHelp] = useState([]);
 
+  const history = useHistory();
+
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
+
 
   useEffect(() => {
     api.get('profile', {
@@ -38,6 +41,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
 
@@ -49,7 +58,7 @@ export default function Profile() {
           {ongName}
         </span>
         <Link className="button" to="/help/new">Register new case</Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#EA80FC" />
         </button>
       </header>
